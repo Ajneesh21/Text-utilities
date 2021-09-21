8765c07fa4d8b97c7c1d74d80125c73841f61cc6
Ajneesh <ajneesh2001@gmail.com>
2021-09-21 10:22:17 +0530

    My first commit

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
 
   const handleOnClick = () => {
-      console.log("UpperCase was clicked");
-      let newText = text.toUpperCase()
-      setText(newText);
-}
+    console.log("UpperCase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    console.log("UpperCase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-  const handleOnChange= (event) => {
-    console.log("UpperCase was c");
+  const handleOnChange = (event) => {
     setText(event.target.value);
-  }
+  };
 
- 
   return (
-      <>
-      <div className="form-group">
+    <>
+      <div className="form-group ">
         <label htmlFor="exampleFormControlTextarea1">
           <h1>{props.heading}</h1>
         </label>
@@ -26,14 +29,27 @@ export default function TextForm(props) {
           value={text}
           className="form-control"
           id="myBox"
-          rows="10"
+          rows="8"
         ></textarea>
       </div>
-      <button onClick={handleOnClick} className="btn btn-primary">Convert to Uppercase</button>
-</>  
+      <button onClick={handleOnClick} className="btn btn-primary mx-2">
+        Convert to Uppercase
+      </button>
+      <button onClick={handleLoClick} className="btn btn-primary mx-2">
+        Convert to Lowercase
+      </button>
+      <div className="form-group my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{Math.ceil(0.008 * text.split(" ").length)} minute(s) read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
-
 
 // function handleSubmit(event) {
 //     event. preventDefault();
